fe8eac5b259399a5403d06794b7762f0881ba171
William Jones <benny@MacBook-Pro-259.local>
2025-04-08 19:20:56 -0400

    Level 4 Complete

--- python_review.py
+++ python_review.py
@@ -3,3 +3,14 @@
 print("it worked!")
 
 x = None
+
+example = "I'm a String" # The variable called example is now holding the string value "I'm a String"
+a = 3 # The variable called a is now holding the integer value 3
+b = 4.0 # The variable called b is now holding the float value 4.0
+c = True # The variable called c is now holding the boolean value True
+d = False # The variable called d is now holding the boolean value False
+e = "Hey!" # The variable called e is now holding the string value "Hey!"
+f = None # The variable called f is now holding no value
+age = 32 # The variable called age is now holding the integer value 32
+name = "William" # The variable called name is now holding the string value "William"
+instrument = "Piano" # The variable called instrument is now holding the string value "Piano"
