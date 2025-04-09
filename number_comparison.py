2f3566397d3eabf3bc12260a3bdf387edecf98d5
William Jones <benny@MacBook-Pro-259.local>
2025-04-08 20:39:20 -0400

    Level 9 Complete

---
+++ number_comparison.py
@@ -0,0 +1,12 @@
+x = input("integer x = ")
+y = input("integer y = ")
+x_int = int(x)
+y_int = int(y)
+
+if (x_int > y_int) == True:
+    print("x > y")
+
+elif (x_int < y_int) == True:
+    print("x < y")
+
+else: print("x = y")
