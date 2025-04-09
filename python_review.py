de3173312fe61eb36151ba6cc044fc44fdd94314
William Jones <benny@MacBook-Pro-259.local>
2025-04-08 20:08:17 -0400

    Level 7 Complete

--- python_review.py
+++ python_review.py
@@ -29,3 +29,38 @@ $$$Variable = "bad"
 x = type(3)
 y = type("Hello")
 z = type(True)
+
+print(x)
+print(y)
+print(z)
+
+my_integer = 42 # Integer
+my_integer_type = type(my_integer)
+print(my_integer_type)
+my_float = 3.14 # Float
+my_float_type = type(my_float)
+print(my_float_type)
+my_string = "Hello, World!" # String
+my_string_type = type(my_string)
+print(my_string_type)
+my_boolean_true = True # Boolean
+my_boolean_true_type = type(my_boolean_true)
+print(my_boolean_true_type)
+my_boolean_false = False # Boolean
+my_boolean_false_type = type(my_boolean_false)
+print(my_boolean_false_type)
+my_none_value = None # None
+my_none_value_type = type(my_none_value)
+print(my_none_value_type)
+my_negative_integer = -7 # Integer
+my_negative_integer_type = type(my_negative_integer)
+print(my_negative_integer_type)
+my_large_float = 1.2345678e10 # Float
+my_large_float_type = type(my_large_float)
+print(my_large_float_type)
+my_single_character = 'A' # String
+my_single_character_type = type(my_single_character)
+print(my_single_character_type)
+my_empty_string = "" # String
+my_empty_string_type = type(my_empty_string)
+print(my_empty_string_type)
